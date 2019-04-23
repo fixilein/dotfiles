98960b9772c6c7f9871fd09a8fdaa0764834e9c3
Felix Tröbinger <felix.troebinger@gmail.com>
2019-04-23 18:14:13 +0200

    added configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+function fish_greeting
+	cat ~/.cache/wal/sequences &
+	neofetch --disable theme icons --disable resolution --color_blocks off
+#	fortune -s -o | sponge | cowsay | lolcat
+	#fish_vi_key_bindings
+end
+
+# add current directory . to PATH
+set -gx PATH $PATH .
+
+alias yeet "yaourt --noconfirm"
+
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+    end
+end
+
+thefuck --alias | source
