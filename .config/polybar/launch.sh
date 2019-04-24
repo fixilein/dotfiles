c741b085c94052dc900d66c32a2cb34dfae8dae5
Felix Tröbinger <felix.troebinger@gmail.com>
2019-04-24 16:35:22 +0200

    started a new polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# Launch bar
 polybar -r example
-# launch the second bar too, if montoir is attached??
+# TODO launch the second bar too, if montoir is attached??
