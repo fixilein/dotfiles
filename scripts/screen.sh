41e104a4cc87b81b4f6aa0d86a3cef41ad64f171
Felix Tröbinger <felix.troebinger@gmail.com>
2019-10-25 21:07:36 +0200

    zsh

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-SC=$(xrandr | grep -sw 'connected' | wc -l) 
-# echo $SC
+SC=$(xrandr | grep -sw 'connected' | wc -l)
 
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+pkill polybar
 polybar bottom &
 
-# ext 
 if test "$SC" -eq 1
 then
   xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 fi
 
 if test "$SC" -eq 2
-then 
+then
 xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
 polybar second &
 
 fi
 
+# restart compton
+pkill compton
+compton -f -b
+
+# start/restart flashfocus
+pkill flashfocus
+flashfocus &
